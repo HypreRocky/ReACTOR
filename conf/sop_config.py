e75f874492120238724c5c9f677703742650c68a
Liangxu Wang <118980353+HypreRocky@users.noreply.github.com>
2026-03-09 16:07:28 +0800

    Update sop_config.py

--- conf/sop_config.py
+++ conf/sop_config.py
@@ -6,9 +6,9 @@ sop_config = {
     "base_dir": BASE_DIR,
     "sops": [
         {
-            "path": "sop/mock_prd_sop.yaml",
-            "triggers": ["存款", "定期", "大额存单", "通知存款", "结构性存款"],
+            "path": "sop/mock_sop.yaml",
+            "triggers": ["", "", "", "", ""],
         },
-        {"path": "sop/mock_loan_sop.yaml"},
+        {"path": "sop/mock_sop.yaml"},
     ],
 }
